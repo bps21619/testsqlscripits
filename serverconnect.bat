cc2fd033d2aadf177462962637f2955fc8738c26
purna <bps21619@gmail.com>
2022-09-27 10:05:30 +0000

    add sub folder2

--- serverconnect.bat
+++ serverconnect.bat
@@ -1,6 +1,6 @@
 @echo run sql scripits
 @echo execute mutiple 
 
-for %%G in (/**/*.sql) do (
-sqlcmd -S EC2AMAZ-VNULPPJ\SQLEXPRESS -E -d bps  -i %%G >> output.txt
+for /r %%G in (*.sql) do (
+sqlcmd -S EC2AMAZ-VNULPPJ\SQLEXPRESS -E -d bps -i %%G >> output.txt
 )
