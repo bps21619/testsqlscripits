3ec1a58f1f5a7f1c2bf33446d12106da85a651cc
purna <bps21619@gmail.com>
2022-09-29 07:10:41 +0000

    purna

--- serverconnect.bat
+++ serverconnect.bat
@@ -1,8 +1,8 @@
 @echo run sql scripits
 @echo execute mutiple 
-for  %cd% in (*.sql) do (
+for  %f in (*.sql) do (
 
-sqlcmd -S EC2AMAZ-VNULPPJ\SQLEXPRESS -U sa -P Sql@2022 -d smartgig -i '%cd%'
+sqlcmd -S EC2AMAZ-VNULPPJ\SQLEXPRESS -U sa -P Sql@2022 -d smartgig -i '%f'
 
 )
 
