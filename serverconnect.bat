5fcbf9c758faa3f3e3e1f7fee81483ec374ea91b
purna <bps21619@gmail.com>
2022-09-29 11:59:38 +0000

    skdjfhbn

--- serverconnect.bat
+++ serverconnect.bat
@@ -1,9 +1,9 @@
 @echo run sql scripits
 @echo execute mutiple 
-SET scriptpath="C:\Users\Administrator\Desktop\sqlscript\"
+::SET scriptpath="C:\Users\Administrator\Desktop\sqlscript\"
 for %%G in (*.sql) do (
 
-sqlcmd /R -S EC2AMAZ-VNULPPJ\SQLEXPRESS -U sa -P Sql@2022 -d "gig" -i "%%G"
+sqlcmd /R -S EC2AMAZ-VNULPPJ\SQLEXPRESS -U purna -P Sql@2022 -d "gig" -i "%%G"
 
 )
 
