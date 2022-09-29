ecbf0730543bb8669bd1b1f8c7136cdd0414fb71
purna <bps21619@gmail.com>
2022-09-29 07:51:07 +0000

    purna

--- serverconnect.bat
+++ serverconnect.bat
@@ -1,7 +1,7 @@
 @echo run sql scripits
 @echo execute mutiple 
 SET scriptpath="C:\Users\Administrator\Desktop\sqlscript\"
-for   %scriptpath% %%G in (*.sql) do (
+for %%G in (*.sql) do (
 
 sqlcmd /R -S EC2AMAZ-VNULPPJ\SQLEXPRESS -U sa -P Sql@2022 -d smartgig -i '%%G'
 
